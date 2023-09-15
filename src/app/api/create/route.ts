7fcb5874b15f240d9b286c1468c1e165636641fa
Samuel Baran <samuel.baran98@gmail.com>
2023-09-15 11:37:42 +0200

    add prisma CLient

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,10 +1,12 @@
 
-import {prisma} from '../../../../lib/prisma'
+import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
  
 type ResponseData = {
   message: string
 }
+
+const prisma = new PrismaClient()
  
 export async function GET(request: Request) {
 
