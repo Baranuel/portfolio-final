6f13296be3ec30d6a358f7119e129854fa322e54
Samuel Baran <samuel.baran98@gmail.com>
2023-09-27 16:46:08 +0200

    hero contraxt text

--- src/app/_Components/HeroSection/Hero.tsx
+++ src/app/_Components/HeroSection/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
     <section className="w-full h-[90vh]  flex flex-col gap-1 items-center justify-center">
    
       <h1 className=" z-10 py-2  text-7xl 2xl:text-6xl sm:text-5xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-b from-primaryGrey/70 from-30% 2xl:from-40% via-primaryBlack/80 via-70% to-primaryBlack/80 to-90%">Web Creator & Design Enthusiast</h1>
-      <p className=" z-10 text-sectionSubtitle text-center  text-primaryGrey/70"> A passionate developer trying to bridge the gap between meaningful and interesting</p>
+      <p className=" z-10 text-sectionSubtitle text-center  text-primaryGrey"> A passionate developer trying to bridge the gap between meaningful and interesting</p>
       <button className=" z-10 px-8 py-2 mt-6 rounded-md bg-primaryBlack text-white">See Projects</button>
     </section>
   );
