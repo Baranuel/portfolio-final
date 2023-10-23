1602dfb8a8f4734a92ff5ca32ef1016a47d388f4
Samuel Baran <samuel.baran98@gmail.com>
2023-10-23 09:20:53 +0200

    use env

--- src/app/axios/axios-instance.ts
+++ src/app/axios/axios-instance.ts
@@ -3,6 +3,6 @@ import axios, { AxiosInstance } from "axios";
 const baseURL = process.env.NODE_ENV === 'production' ? 'https://portfolio-final-khaki.vercel.app/api/' : 'http://localhost:3000/api/';
 
 export const axiosInstance:AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: baseURL,
     timeout: 20000,
   });
