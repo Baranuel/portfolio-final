29d8b1d9e30d6a091b30cc6e6fa3ca3e32a94afe
Samuel Baran <samuel.baran98@gmail.com>
2023-10-23 12:02:19 +0200

    www

--- src/app/axios/axios-instance.ts
+++ src/app/axios/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://samuelbaran.dev/api/' : 'http://localhost:3000/api/';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://www.samuelbaran.dev/api/' : 'http://localhost:3000/api/';
 
 export const axiosInstance:AxiosInstance = axios.create({
     baseURL: baseURL,
