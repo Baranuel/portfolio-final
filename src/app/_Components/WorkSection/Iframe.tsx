ab4d52eec65676a71a301911848fd9c8234ee927
Samuel Baran <samuel.baran98@gmail.com>
2023-10-27 13:13:55 +0200

    add loading to the preview project

--- src/app/_Components/WorkSection/Iframe.tsx
+++ src/app/_Components/WorkSection/Iframe.tsx
@@ -1,5 +1,20 @@
+'use client'
+
+import react, { useState } from "react"
+import { Spinner } from "../Spinner/Spinner"
 
 export const Iframe = ({src}: {src:string}) => {
 
-    return <iframe  loading="eager" id='iframe' src={src} className="w-full h-[calc(100%-40px)]"/>
+    const [loaded, setLoaded] = useState(false)
+
+    return (
+        <>
+        {!loaded &&  <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+            <Spinner size='extraLarge'/>
+            <span className="text-white">Loading Preview...</span>
+        </div>}
+        <iframe className={`w-full h-full ${loaded ? 'block' : 'hidden'}`} src={src} onLoad={() => setLoaded(true)}></iframe>
+        </>
+       
+    )
 }
